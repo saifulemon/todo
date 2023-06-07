814d06edc212661eef2e584df5c6fa55fc1301e4
saifulemon <463saiful0@gmail.com>
2023-06-07 10:51:28 +0600

    initial commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Home() {
+
+    const todos = ["todo1", "todo2", "todo3", "todo4"];
+
+  return (
+    <>
+        <h1>I am from Home</h1>
+    </>
+  )
+}
+
+export default Home
---
+++ src/components/Todos/Todos.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Todos() {
+  return (
+    <>
+        
+    </>
+  )
+}
+
+export default Todos
